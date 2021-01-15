866ab2da9cfd60847664cb286b8f16d399b915e3
panp.zhang001 <panp.zhang001@bkjk.com>
2021-01-15 16:09:07 +0800

    react-beautiful-dnd

--- src/container/cart/index.jsx
+++ src/container/cart/index.jsx
@@ -1,15 +1,33 @@
 import React from "react";
 import "./style.css";
-import { Card } from "antd";
-
+// import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+const data = [
+  'Racing car sprays burning fuel into crowd.',
+  'Japanese princess to wed commoner.',
+  'Australian walks 100km after outback crash.',
+  'Man charged over missing wedding girl.',
+  'Los Angeles battles huge wildfires.',
+];
 const Cart = () => {
+  const handleDragEnd = (result) => {
+    console.log("拖拽结束", result);
+  };
+  const handleDragStart = (result) => {
+    console.log("拖拽开始", result);
+  };
   return (
     <div className="cart-container">
-      <Card title="购物车">
-
-      </Card>
+      {/* <DragDropContext onDragEnd={handleDragEnd} onDragStart={handleDragStart}>
+        <div className="left">
+          <Droppable droppableId="left-title-container">
+          </Droppable>
+        </div>
+      </DragDropContext> */}
     </div>
   )
 };
 
 export default Cart;
+
+const arr = [1, 2, 3];
+console.log(...arr)
